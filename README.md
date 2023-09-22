c7dd43ee4c839ae4491c118267d9248d89425011
Emiliano Rivera <a358193@uach.mx>
2023-09-22 16:16:57 -0600

    Agregue informacion al README, agregue algunas opciones al main y agregue 7 archivos inf con sus respectivos identificadores y definiciones

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# projectDBP
+# ProjectDBP
+
+Realizamos una aplicación que nos ayudara a aprender acerca de las diferentes metodologías de desarrollo ágil de software
+
+## Empezando
+
+Aqui te mostramos las instrucciones para realizar una copia del proyecto para pruebas
+
+### Prerequisitos
+
+Primero necesitas instalar ciertas herramientas
+
+```
+Github
+Git
+Docker
+VsCode
+```
+
+### Instalando en Debian
+
+Para instalar Git
+
+```
+sudo apt install git
+```
+
+Para instalar Docker
+
+```
+sudo apt-get install docker-ce
+```
+
+## Autores
+
+* **Cristian Yael Rubi Loera**
+* **Emiliano Rivera Armendariz** 
+* **Josue David Raiz Almeida**
--- main.sh
+++ main.sh
@@ -2,20 +2,76 @@ main(){
     #Variables de entorno
     running=true
     inputMain=''
+    opc=0
+    opc2=0
     #Logica de ejecucion del menu
     while [[ $running = true ]]; do
-        read -p "Ingresa -a para entrar al menu de metodología ágil o -t para entrar a la metodología tradicional" inputMain        
+        read -p "Ingresa -a para entrar al menu de metodología ágil o -t para entrar a la metodología tradicional: " inputMain        
         if [ $inputMain = "-a" ]; then
-            echo "Menú de metodología ágil"
+            echo "Bienvenido a la guia rapida de Agile, para continuar seleccione un tema: "
+            echo "1. SCRUM"
+            echo "2. X.P."
+            echo "3. Kanban"
+            echo "4. Crystal"
+            read -p "Opcion: " opc
+            case $opc in
+                1 )
+                echo "Usted esta en la seccion SCRUM, seleccione la opcion que desea utilizar"
+                    ;;
+                2 )
+                echo "Usted esta en la seccion X.P, seleccione la opcion que desea utilizar"
+                    ;;
+                3 )
+                echo "Usted esta en la seccion Kanban, seleccione la opcion que desea utilizar"
+                    ;;
+                4 )
+                echo "Usted esta en la seccion Crystal, seleccione la opcion que desea utilizar"
+                    ;;
+            esac
             # To-Do: Falta función para desplegar el menu de metodoloǵia ágil
         elif [ $inputMain = "-t" ]; then
-            echo "Menú de metodología tradicional"
+            echo "Bienvenido a la guia rapida de metodologias tradicionales, para continuar seleccione un tema: "
+            echo "1. Cascada"
+            echo "2. Espiral"
+            echo "3. Modelo V"
+            read -p "Opcion: " opc2
+            case $opc2 in
+                1 )
+                echo "Usted esta en la seccion Cascada, seleccione la opcion que desea utilizar"
+                    ;;
+                2 )
+                echo "Usted esta en la seccion Espiral, seleccione la opcion que desea utilizar"
+                    ;;
+                3 )
+                echo "Usted esta en la seccion Modelo V, seleccione la opcion que desea utilizar"
+                    ;;
+            esac
             # To-Do: Falta función para desplegar el menu de metodoloǵia tradicional
         else
             running=false
         fi
     done
 }
+
+: '
+
+agregarInfo(){
+
+}
+
+buscar(){
+
+}
+
+eliminarInfo(){
+
+}
+
+leerBaseInfo(){
+
+}
+
+'
 
 #Disparador de la función principal de ejecucion del programa
 main
