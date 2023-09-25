1c5d1d0c3895b6ca292a65250bba3cac1f54b461
David Almeida <a358472@uach.mx>
2023-09-25 01:44:56 -0600

    Agregue La Funcion del Menu Metodologia y La Funcion De Agregar Informacion. [NOTA: Checar los comentarios en el codigo]

--- main.sh
+++ main.sh
@@ -4,31 +4,48 @@ main(){
     inputMain=''
     opc=0
     opc2=0
+    opcSelected=''
+
     #Logica de ejecucion del menu
     while [[ $running = true ]]; do
         read -p "Ingresa -a para entrar al menu de metodología ágil o -t para entrar a la metodología tradicional: " inputMain        
         if [ $inputMain = "-a" ]; then
+            echo "--------------------------------------------------------------------------"
             echo "Bienvenido a la guia rapida de Agile, para continuar seleccione un tema: "
             echo "1. SCRUM"
             echo "2. X.P."
             echo "3. Kanban"
             echo "4. Crystal"
             read -p "Opcion: " opc
+            echo "--------------------------------------------------------------------------"
             case $opc in
                 1 )
-                echo "Usted esta en la seccion SCRUM, seleccione la opcion que desea utilizar"
+                echo "Usted esta en la seccion SCRUM, seleccione la opcion que desea utilizar:"
+                opcSelected="SCRUM"
+                menuOpciones "$opcSelected"
                     ;;
                 2 )
                 echo "Usted esta en la seccion X.P, seleccione la opcion que desea utilizar"
+                opcSelected="X.P"
+                menuOpciones "$opcSelected"
                     ;;
                 3 )
                 echo "Usted esta en la seccion Kanban, seleccione la opcion que desea utilizar"
+                opcSelected="Kanban"
+                menuOpciones "$opcSelected"
                     ;;
                 4 )
                 echo "Usted esta en la seccion Crystal, seleccione la opcion que desea utilizar"
+                opcSelected="Crystal"
+                menuOpciones "$opcSelected"
+                    ;;
+                * )
+                #Este clear no supe si dejarlo o no, ustedes deciden.
+                clear
+                echo "Opcion No valida, Digite Nuevamente."
                     ;;
             esac
-            # To-Do: Falta función para desplegar el menu de metodoloǵia ágil
+
         elif [ $inputMain = "-t" ]; then
             echo "Bienvenido a la guia rapida de metodologias tradicionales, para continuar seleccione un tema: "
             echo "1. Cascada"
@@ -38,32 +55,166 @@ main(){
             case $opc2 in
                 1 )
                 echo "Usted esta en la seccion Cascada, seleccione la opcion que desea utilizar"
+                opcSelected="Cascada"
+                menuOpciones "$opcSelected"
                     ;;
                 2 )
                 echo "Usted esta en la seccion Espiral, seleccione la opcion que desea utilizar"
+                opcSelected="Espiral"
+                menuOpciones "$opcSelected"
                     ;;
                 3 )
                 echo "Usted esta en la seccion Modelo V, seleccione la opcion que desea utilizar"
+                opcSelected="ModeloV"
+                menuOpciones "$opcSelected"
                     ;;
+                * )
+                #Este clear no supe si dejarlo o no, ustedes deciden.
+                clear
+                echo "Opcion No valida, Digite Nuevamente."
+                    ;;  
             esac
-            # To-Do: Falta función para desplegar el menu de metodoloǵia tradicional
+
         else
             running=false
         fi
     done
 }
 
-: '
 
-agregarInfo(){
+menuOpciones(){
+    #Varibles de Entorno
+    opcSelected="$1"
+    opc=0
+
+    #Menu de opciones
+    echo "1. Agregar Informacion"
+    echo "2. Buscar"
+    echo "3. Eliminar Informacion"
+    echo "4. Leer Base de Datos"
+    read -p "Opcion: " opc
+    echo "--------------------------------------------------------------------------"
+
+    #Case opciones Menu
+    case $opc in
+        1 )
+        echo "Ha Seleccionado La Opcion Agregar Informacion"
+        agregarInfo "$opcSelected"
+            ;;
+        2 )
+        #ToDo: Completar el case.
+        echo "-----------------"
+        #buscar "$opcSelected"
+            ;;
+        3 )
+        echo "------------------"
+        #eliminarInfo "opcSelected"
+            ;;
+    esac
 
 }
 
+agregarInfo(){
+    #Variables de entorno
+    opcSelected="$1"
+    concept=''
+    info=''
+
+    #IFs para cada opcion
+    if [ $opcSelected = "SCRUM" ]; then
+        #Lee contendio del archivo
+        archive=$(cat SCRUM.inf)
+        #Solicita el concepto
+        read -p "Digite El Nombre Del Nuevo Concepto: " concept
+        #Solicita nueva informacion
+        read -p "Introduzca La Nueva Informacion: " info
+        #Agrega nueva informacion y actualizarlo
+        echo -e "[$concept] .- $info\n" >> SCRUM.inf
+        echo "Se Agrego Informacion Correctamente"
+    fi
+
+    if [ $opcSelected = "X.P" ]; then
+        #Lee contendio del archivo
+        archive=$(cat XP.inf)
+        #Solicita el concepto
+        read -p "Digite El Nombre Del Concepto: " concept
+        #Solicita nueva informacion
+        read -p "Introduzca La Nueva Informacion: " info
+        #Agrega nueva informacion y actualizarlo
+        echo -e "[$concept] .- $info\n" >> XP.inf
+        echo "Se Agrego Informacion Correctamente"
+    fi
+
+    if [ $opcSelected = "Kanban" ]; then
+        #Lee contendio del archivo
+        archive=$(cat Kanban.inf)
+        #Solicita el concepto
+        read -p "Digite El Nombre Del Concepto: " concept
+        #Solicita nueva informacion
+        read -p "Introduzca La Nueva Informacion: " info
+        #Agrega nueva informacion y actualizarlo
+        echo -e "[$concept] .- $info\n" >> Kanban.inf
+        echo "Se Agrego Informacion Correctamente"
+    fi
+
+    if [ $opcSelected = "Crystal" ]; then
+        #Lee contendio del archivo
+        archive=$(cat Crystal.inf)
+        #Solicita el concepto
+        read -p "Digite El Nombre Del Concepto: " concept
+        #Solicita nueva informacion
+        read -p "Introduzca La Nueva Informacion: " info
+        #Agrega nueva informacion y actualizarlo
+        echo -e "[$concept] .- $info\n" >> Crystal.inf
+        echo "Se Agrego Informacion Correctamente"
+    fi
+
+    if [ $opcSelected = "Cascada" ]; then
+        #Lee contendio del archivo
+        archive=$(cat Cascada.inf)
+        #Solicita el concepto
+        read -p "Digite El Nombre Del Concepto: " concept
+        #Solicita nueva informacion
+        read -p "Introduzca La Nueva Informacion: " info
+        #Agrega nueva informacion y actualizarlo
+        echo -e "[$concept] .- $info\n" >> Cascada.inf
+        echo "Se Agrego Informacion Correctamente"
+    fi
+
+    if [ $opcSelected = "Espiral" ]; then
+        #Lee contendio del archivo
+        archive=$(cat Espiral.inf)
+        #Solicita el concepto
+        read -p "Digite El Nombre Del Concepto: " concept
+        #Solicita nueva informacion
+        read -p "Introduzca La Nueva Informacion: " info
+        #Agrega nueva informacion y actualizarlo
+        echo -e "[$concept] .- $info\n" >> Espiral.inf
+        echo "Se Agrego Informacion Correctamente"
+    fi
+
+    if [ $opcSelected = "ModeloV" ]; then
+        #Lee contendio del archivo
+        archive=$(cat ModeloV.inf)
+        #Solicita el concepto
+        read -p "Digite El Nombre Del Concepto: " concept
+        #Solicita nueva informacion
+        read -p "Introduzca La Nueva Informacion: " info
+        #Agrega nueva informacion y actualizarlo
+        echo -e "[$concept] .- $info\n" >> ModeloV.inf
+        echo "Se Agrego Informacion Correctamente"
+    fi
+}
+
+
+: '
 buscar(){
 
 }
 
 eliminarInfo(){
+    
+    #newinfo=$(echo "$archive" | sed "s/\[$concept\].*/[$concept] .- $info/") [=======  NO ELIMINAR, posible uso para un fututo. ===========]
 
 }
 
